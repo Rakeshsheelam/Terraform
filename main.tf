206ac13817a8cd01d20ff01d40087f35bec412ca
Rakeshsheelam <rakeshsheelam98@outlook.com>
2024-12-06 18:19:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,31 @@
-resource "local_file" "my_pet" {
-  filename = var.filename
-  content  = var.content
+terraform {
+  required_providers {
+    local = {
+      source = "hashicorp/local"
+      version = "<2.5.0"
+    }
+  }
 }
 
-resource "random_pet" "pet" {
-  prefix    = var.prefix
-  separator = "."
-  length    = "1"
+resource "local_file" "my_pet" {
+  filename = "pets.txt"
+  content  = "my pet name is ${random_pet.racey.id}"
+  
 }
+
+resource "random_pet" "racey" {
+  prefix    = "MR"
+  separator = "."
+  length    = "2"
+
+resource "local_file" "data" {
+  filename = "pets.txt"
+  content  = data.local_file.dog.content
+
+}
+
+data "local_file" "dog" {
+  filename = "dog.txt"
+}
+
+
