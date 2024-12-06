9eea57aaefac0b0d202b90da48684ed9d82a3e9e
Rakeshsheelam <rakeshsheelam98@outlook.com>
2024-12-06 12:46:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
 resource "local_file" "my_pet" {
-filename = "dog.txt"
-content = "My job name is racey!"
+  filename = var.filename
+  content  = var.content
 }
-resource "random_pet" "my_pet" {
-prefix = "MR"
-separator = "."
-length = "1"
+
+resource "random_pet" "pet" {
+  prefix    = "MR"
+  separator = "."
+  length    = "1"
 }
