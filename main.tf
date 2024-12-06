6fea7e717cc6fdeb37d9a6f0f5f8691c0625ea5f
Rakeshsheelam <rakeshsheelam98@outlook.com>
2024-12-06 18:26:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,26 +6,3 @@ terraform {
     }
   }
 }
-
-resource "local_file" "my_pet" {
-  filename = "pets.txt"
-  content  = "my pet name is ${random_pet.racey.id}"
-  
-}
-
-resource "random_pet" "racey" {
-  prefix    = "MR"
-  separator = "."
-  length    = "2"
-
-resource "local_file" "data" {
-  filename = "pets.txt"
-  content  = data.local_file.dog.content
-
-}
-
-data "local_file" "dog" {
-  filename = "dog.txt"
-}
-
-
